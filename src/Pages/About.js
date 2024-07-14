cc31c35edca8463172659a097275b494c7160bf2
LaraibAzmat <azmatlaraib2003@gmail.com>
2024-07-14 18:05:55 +0500

    about us responsive

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -11,31 +11,25 @@ export default function About() {
   }}>
         <span className="text-red-500"> About</span> Us
       </h2>
-      <div className="flex px-20 md:px-20 md:gap-70 vision-container ">
-        <div className="pt-4 md:w-2/3">
-          <h3 className="py-4 text-xl font-bold ">Our Mission & Vision</h3>
-          <span
-            className="block w-[300px] h-px  bg-gray-700"
-            aria-hidden="true"
-          ></span>
-          <p className="py-4 text-xl">
-            Our Mission is make hight quality education, accessible to
-            everyone, everywhere.
-          </p>
-          <p className="py-4 text-xl">
-            We envision a World where learning is a lifelong journey and
-            opportunities for growth are available to all.
-          </p>
-        </div>
-        <div className="" style={{marginLeft:"8 rem"}}>
-          <img
-            src={missionImg}
-            className="w-[350px] mt-8 ml-32 sm:ml-8 md:ml-2 sm:mx-auto sm:mt-4"
-            
-            alt="Trac Logo"
-          />
-        </div>
-      </div>
+      <div className="flex flex-col md:flex-row  px-4 md:px-20 gap-4 md:gap-8 vision-container">
+  <div className="pt-4 md:w-2/3">
+    <h3 className="py-4 text-xl font-bold">Our Mission & Vision</h3>
+    <span className="block w-[300px] h-px bg-gray-700" aria-hidden="true"></span>
+    <p className="py-4 text-xl">
+      Our Mission is to make high quality education accessible to everyone, everywhere.
+    </p>
+    <p className="py-4 text-xl">
+      We envision a world where learning is a lifelong journey and opportunities for growth are available to all.
+    </p>
+  </div>
+  <div className="md:ml-8">
+    <img
+      src={missionImg}
+      className="w-[350px] mt-8 sm:mx-auto sm:mt-4"
+      alt="Trac Logo"
+    />
+  </div>
+</div>
       <p className="px-20 py-4 text-xl md:px-20">
         We combine cutting-edge technology with industry expertise to deliever
         engaging, interactive and affective learning experiences.Our courses
